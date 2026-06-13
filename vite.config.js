7c47fd98c834189bc887801aa07b76848408b3a8
piush365 <piushgogi@email.com>
2026-06-13 13:55:07 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig(({ command, mode }) => ({
-  plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/Diploma-Dost/' : '/',
-}))
